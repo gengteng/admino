1f91edc296442ac39c3928c1eb98f2930ecfd9ad
gengteng <me@gteng.org>
2020-02-17 09:53:52 +0800

    修改 IntoJsonResult 的错误类型，使之适用于更多错误类型

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -6,12 +6,12 @@ use actix_web::web::Json;
 use deadpool_postgres::Pool as PgPool;
 use deadpool_redis::Pool as RedisPool;
 
-pub trait IntoJsonResult<T> {
+pub trait IntoJsonResult<T, E: Into<Error>> {
     fn json(self) -> Result<Json<T>, Error>;
 }
 
-impl<T> IntoJsonResult<T> for Result<T, Error> {
+impl<T, E: Into<Error>> IntoJsonResult<T, E> for Result<T, E> {
     fn json(self) -> Result<Json<T>, Error> {
-        self.map(Json)
+        self.map(Json).map_err(E::into)
     }
 }
